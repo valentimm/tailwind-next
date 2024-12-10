566a19d17525d446720ac48c031c24b5b94b124a
Matheus Valentim <meduardo.valentim@gmail.com>
2024-12-09 23:57:51 -0300

    feat: create new grid  profile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ export default {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
+        profile: 'max-content 1fr min-content',
       },
       colors: {
         background: 'var(--background)',
