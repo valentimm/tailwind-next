f878fec487854d0f432ded06885fdd6ea3aaca74
Matheus Valentim <meduardo.valentim@gmail.com>
2024-12-11 23:34:32 -0300

    feat: create tab items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { SettingsTabs } from './components/SettingsTabs'
+
 export default function Home() {
-  return <h1>Oi</h1>
+  return (
+    <>
+      <h1 className="font-md text-3xl text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+    </>
+  )
 }
